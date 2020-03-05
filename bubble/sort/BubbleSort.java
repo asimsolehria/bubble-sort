2df0702f0946b2916a3aadf9488705ca5def30e3
Asim Habib <de.asimhabib@gmail.com>
2020-03-05 14:06:14 +0500

    Added an array

--- bubble/sort/BubbleSort.java
+++ bubble/sort/BubbleSort.java
@@ -4,18 +4,21 @@
  * and open the template in the editor.
  */
 package bubble.sort;
-
 /**
  *
  * @author Asim
  */
 public class BubbleSort {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int[]array={12,98,45,75,12,87,102,40,19,478};
+        System.out.println("List of unsorted array elements:");
+        
+        
     }
     
 }
