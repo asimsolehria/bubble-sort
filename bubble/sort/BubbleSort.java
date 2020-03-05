ed2327757ab1ad2e2578792671d98876608cfb2d
Asim Habib <de.asimhabib@gmail.com>
2020-03-05 17:01:18 +0500

    Added methods of sorting and print array

--- bubble/sort/BubbleSort.java
+++ bubble/sort/BubbleSort.java
@@ -15,10 +15,31 @@ public class BubbleSort {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[]array={12,98,45,75,12,87,102,40,19,478};
-        System.out.println("List of unsorted array elements:");
-        
-        
+        int[]array={12,98,45,75,13,87,102,40,19,478};
+        System.out.println("Unsorted array elements!");
+        printArray(array);
+        Sort(array);
+        System.out.println("Sorted array elements!");
+        printArray(array);
     }
     
+    static void Sort(int[]array)
+    {
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length-1-i; j++) {
+                if(array[j]>array[j+1]){
+                    int temp;
+                    temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+    }
+    static void printArray(int[]array){
+        for(int element:array)
+        {
+            System.out.println(element);
+        }
+    }
 }
